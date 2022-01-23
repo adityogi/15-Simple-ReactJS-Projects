ad1a91a6ece5ddcc5d08d92176f622184c4ef7d2
adityogi <73770862+adityogi@users.noreply.github.com>
2022-01-23 16:32:32 +0530

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import useSWR from 'swr'
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
 import Property from '../components/Property';
